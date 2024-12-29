6556a42a40452dc8a460795e4bc41b35c1938027
subanah <162430575+subanah@users.noreply.github.com>
2024-12-29 22:20:35 +0600

    Add files via upload

---
+++ neww.js
@@ -0,0 +1,6 @@
+let NEWSLATTERBODY = document.querySelector("#newsLatterBody");
+let KOROSS = document.querySelector("#dftnihdyb");
+
+KOROSS.addEventListener("click", function() {
+  NEWSLATTERBODY.style.display = "none";
+});
